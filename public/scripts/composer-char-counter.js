e200cc2fc4376d57dac5eff22b0bb8c0acb106ad
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-06-21 19:43:48 +0000

    fixed counter function by moving it below the other script

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,14 @@
+debugger;
 $(document).ready(function () {
   var counter = $('.counter');
+  console.log ("this is a console log", "1")
   var tweet = $("textarea[name=text]");
   tweet.on('input', function (event) {
+    console.log ("this is a console log", "2")
     counterNum = (140 - event.target.value.length);
     counter.text(counterNum);
-
-
-    var color = 'black';
-    if (counterNum < 0) {
-      color = 'red';
-    }
-counter.css({color: color});
+    var color = counterNum < 0 ? "red" : "black";
+    counter.css({ color });
   });
 });
+
